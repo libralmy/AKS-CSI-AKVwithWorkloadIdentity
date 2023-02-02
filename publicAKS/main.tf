d021e1c48e3466b635cd09172e45a286a326b2d0
Emily Li <libralmy1003@gmail.com>
2023-02-02 10:50:58 -0500

    pub aks

---
+++ publicAKS/main.tf
@@ -0,0 +1,63 @@
+data "azurerm_resource_group" "main" {
+  name = var.resource_group_name
+}
+
+data "http" "myip" {
+  url = "https://ipv4.icanhazip.com"
+}
+
+resource "azurerm_user_assigned_identity" "aks" {
+  location            = data.azurerm_resource_group.main.location
+  name                = var.cluster_identity
+  resource_group_name = data.azurerm_resource_group.main.name
+  tags                = var.tags
+}
+
+# resource "azurerm_role_assignment" "aks_network_contributor" {
+#   principal_id         = azurerm_user_assigned_identity.aks.principal_id
+#   scope                = azurerm_virtual_network.main.id
+#   role_definition_name = "Network Contributor"
+# }
+
+resource "azurerm_kubernetes_cluster" "main" {
+  location            = data.azurerm_resource_group.main.location
+  name                = var.cluster_name
+  resource_group_name = data.azurerm_resource_group.main.name
+  # Allow the current client's public IP address only
+  api_server_authorized_ip_ranges = ["${chomp(data.http.myip.response_body)}/32"]
+  dns_prefix                      = var.cluster_dns_prefix
+  oidc_issuer_enabled             = true
+  private_cluster_enabled         = false
+  sku_tier                        = var.cluster_sku_tier
+  tags                            = var.tags
+  workload_identity_enabled       = true
+
+  default_node_pool {
+    name           = "default"
+    vm_size        = var.default_node_pool_vm_size
+    node_count     = 1
+    tags           = var.tags
+    # vnet_subnet_id = azurerm_subnet.main.id
+  }
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.aks.id]
+  }
+
+  key_vault_secrets_provider {
+    secret_rotation_enabled = true
+  }
+  azure_policy_enabled              = true
+  http_application_routing_enabled  = false
+  role_based_access_control_enabled = true
+
+  # network_profile {
+  #   network_plugin     = "azure"
+  #   dns_service_ip     = var.cluster_dns_service_ip_address
+  #   docker_bridge_cidr = var.cluster_docker_bridge_address
+  #   service_cidr       = var.cluster_service_ip_range
+  # }
+}
+
+
---
+++ publicAKS/outputs.tf
@@ -0,0 +1,34 @@
+# output "api_server_authorized_ip_ranges" {
+#   value       = "${chomp(data.http.myip.response_body)}/32"
+#   description = "CIDR range of IP addresses that can access the cluster's API server's public endpoint"
+# }
+
+output "credentials_command" {
+  value       = "az aks get-credentials --resource-group ${data.azurerm_resource_group.main.name} --name ${azurerm_kubernetes_cluster.main.name}"
+  description = "Command to get the cluster's credentials with az cli"
+}
+
+output "id" {
+  value       = azurerm_kubernetes_cluster.main.id
+  description = "ID of the created kubernetes cluster"
+}
+
+output "invoke_command" {
+  value       = "az aks command invoke --resource-group ${data.azurerm_resource_group.main.name} --name ${azurerm_kubernetes_cluster.main.name} --command \"kubectl get pods -n kube-system\""
+  description = "Sample command to execute k8s commands on the cluster using az cli"
+}
+
+output "name" {
+  value       = azurerm_kubernetes_cluster.main.name
+  description = "Name of the kubernetes cluster created by this module"
+}
+
+output "oidc_issuer_url" {
+  value       = azurerm_kubernetes_cluster.main.oidc_issuer_url
+  description = "OIDC Issuer URL for the public cluster"
+}
+
+output "resource_group_name" {
+  value       = azurerm_kubernetes_cluster.main.resource_group_name
+  description = "Name of the resource group where the cluster was created"
+}
